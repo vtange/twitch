9a9319f54a400f278abb70afecd405bdc60eff64
Victor Tang <victortang1992@gmail.com>
2015-11-05 20:57:42 -0800

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+(function() {
+    //start of function
+  var app = angular.module('Menus', []);
+
+  app.controller('ThreePartList', function() {
+    this.products = gems;
+  });
+  //end of function
+})();
