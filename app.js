bc538821c7a44b4bda155cd8c78d817d7bab6d5e
Victor Tang <victortang1992@gmail.com>
2015-11-06 02:00:53 -0800

    angular init, 'sera' ng-model works

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 (function() {
     //start of function
-  var app = angular.module('Menus', []);
+  var app = angular.module('ChannelSearcher', []);
 
-  app.controller('ThreePartList', function() {
-    this.products = gems;
-  });
+
+app.factory('memory', [function(){
+  var storage = {
+  };
+  return storage;
+}]);//end of service
+
+app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
+    $scope.storage = memory; // load service
+    $scope.print = function(){
+        console.log($scope.sera)   
+    }
+    
+}]);//end of controller
   //end of function
 })();
