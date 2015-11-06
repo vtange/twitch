36a19e194a09980ac2b6edb25db876c14262f2ae
Victor Tang <victortang1992@gmail.com>
2015-11-06 12:38:01 -0800

    load twitch api into service

--- app.js
+++ app.js
@@ -3,11 +3,21 @@
   var app = angular.module('ChannelSearcher', []);
 
 
-app.factory('memory', [function(){
-  var storage = {
-  };
+app.factory('memory', function($http){
+  var storage = {}
+
+  storage.the_good_stuff = {};
+
+    var url = "https://api.twitch.tv/kraken/streams?channel=freecodecamp&callback=JSON_CALLBACK";
+    $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
+    $http.jsonp(url).success(function(data) {
+       storage.the_good_stuff = data;
+    }).error(function(data) {
+       storage.the_good_stuff = {};
+    });
+
   return storage;
-}]);//end of service
+});//end of service
 
 app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
     $scope.storage = memory; // load service
