4af0bb059692e3bf1ff6b46f65ba9c6869f35d49
Victor Tang <victortang1992@gmail.com>
2015-11-06 16:06:06 -0800

    cycle http requests through a list of usernames

--- app.js
+++ app.js
@@ -2,19 +2,35 @@
     //start of function
   var app = angular.module('ChannelSearcher', []);
 
-
 app.factory('memory', function($http){
-  var storage = {}
 
-  storage.the_good_stuff = {};
+  var baseUrl = "https://api.twitch.tv/kraken/";
 
-    var url = "https://api.twitch.tv/kraken/streams/freecodecamp.json";
-    $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-    $http.jsonp(url + "?callback=JSON_CALLBACK").success(function(data) {
-       storage.the_good_stuff = data;
-    }).error(function(data) {
-       storage.the_good_stuff = {};
-    });
+  var users = ["freecodecamp","tentuzero","watchgintama","lordstormakov"];
+
+  var storage = {};
+  storage.profileInfos = [];
+  storage.onlineStatuses = [];
+
+    for (var i=0;i<users.length;i++){
+       var profileExt = "users/";
+       var statusExt = "streams/";
+
+        $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
+        $http.jsonp(baseUrl + profileExt + users[i] + "?callback=JSON_CALLBACK").success(function(data) {
+           storage.profileInfos.push(data);
+        }).error(function(data) {
+           storage.profileInfos = [];
+           console.log("error0");
+        });
+        $http.jsonp(baseUrl + statusExt + users[i] + "?callback=JSON_CALLBACK").success(function(data) {
+           storage.onlineStatuses.push(data);
+        }).error(function(data) {
+           storage.onlineStatuses = [];
+           console.log("error1");
+        });
+
+    }
 
   return storage;
 });//end of service
@@ -22,9 +38,9 @@ app.factory('memory', function($http){
 app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
     $scope.storage = memory; // load service
     $scope.print = function(){
-        console.log($scope.storage.the_good_stuff)
+        console.log($scope.storage.profileInfos)
     }
-    
+
 }]);//end of controller
   //end of function
 })();
